edc41af179b1aaf999c4011228580f1672ea3eb5
Aman K. Azad <amanazad38@gmail.com>
2017-08-01 23:47:29 +0530

    Update Floyd_Warshall.cpp

--- Floyd_Warshall.cpp
+++ Floyd_Warshall.cpp
@@ -4,8 +4,8 @@ void Floyd-Warshal(){
   for(int i=0;i<n;i++) d[v][v] = 0 ;
     
     
-    for(int k=0;k<n;k++) 
-     for(int i=0;i<n;i++) 
+    for(int k=0;k<n;k++)   // Pick all vertices as source one by one.
+     for(int i=0;i<n;i++)  // Pick all vertices as destination one by one.
         for(int j=0;j<n;j++) 
           d[i][j]=min(d[i][j],d[i][k]+d[k][j]);
         
